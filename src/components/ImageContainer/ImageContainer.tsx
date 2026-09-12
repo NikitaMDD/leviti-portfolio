73c53b48c6eb30f2c605d10e50f60220ac17801e
nikita <nikitamarch702@gmail.com>
2026-09-12 12:33:52 +0300

    feat: обновляет обертку картинок делаем упор на диджейскую эстетику

--- src/components/ImageContainer/ImageContainer.tsx
+++ src/components/ImageContainer/ImageContainer.tsx
@@ -5,19 +5,22 @@ interface ImageContainerProps {
     src: string;
     description?: string;
     alt?: string;
+    objectPosition?: string;
 }
 
 export const ImageContainer = ({
     src,
     alt,
     description,
+    objectPosition,
 }: ImageContainerProps) => {
     return (
         <div className={`${classes.imageWrapper} js-about-photo`}>
-            <img src={src} alt={alt} />
+            <img src={src} alt={alt} style={objectPosition ? { objectPosition } : undefined} />
             {
                 description ?
                     <div className={classes.photoClue}>
+                        <span className={classes.photoClueDot} />
                         {description}
                     </div>
                     : null
